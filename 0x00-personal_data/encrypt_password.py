17bb0bd993ac07bbb3e0b9bd182932a78e08a708
THOSSYN <alextosin5@gmail.com>
2024-02-08 18:00:26 +0100

    Added README, filtered_logger.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,4 +14,4 @@ def hash_password(password: str) -> bytes:
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Checks if a provided password matches bcrypt hashes"""
     password = password.encode('utf-8')
-    return bcrypt.checkpwd(password, hashed_password)
+    return bcrypt.checkpw(password, hashed_password)
