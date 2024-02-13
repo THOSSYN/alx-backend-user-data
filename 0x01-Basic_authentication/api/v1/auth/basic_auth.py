821a8569f0d9ba5db8ba90cff5c9fcad823ffbe3
THOSSYN <alextosin5@gmail.com>
2024-02-13 13:39:40 +0100

    Added api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -29,3 +29,15 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except ValueError:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """Extracts decoded authorization header"""
+        if decoded_base64_authorization_header is None or \
+                not isinstance(decoded_base64_authorization_header, str) or \
+                ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, pwd = decoded_base64_authorization_header.split(':')
+        return email, pwd
