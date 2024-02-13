3fa94de27693a1714b16d04b40cd5731f1421758
THOSSYN <alextosin5@gmail.com>
2024-02-13 14:07:33 +0100

    Added api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,7 +41,7 @@ class BasicAuth(Auth):
                 not isinstance(decoded_base64_authorization_header, str) or \
                 ':' not in decoded_base64_authorization_header:
             return None, None
-        email, pwd = decoded_base64_authorization_header.split(':')
+        email, pwd = decoded_base64_authorization_header.split(':', 1)
         return email, pwd
 
     def user_object_from_credentials(
