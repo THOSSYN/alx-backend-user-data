8512395758772c80b1ed70aeb19ff7bbe5dacea7
THOSSYN <alextosin5@gmail.com>
2024-02-13 11:40:08 +0100

    Updated api/v1/views/index.py for 401, 403 error handling

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,19 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Handle 401 error"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    """Handles 403 error"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
