41eac2b9bda5ad3f6b55c25e36551ba16f9fc72d
THOSSYN <alextosin5@gmail.com>
2024-02-22 08:43:31 +0100

    Updated app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,7 +11,7 @@ AUTH = Auth()
 @app.route('/', strict_slashes=False)
 def root() -> Response:
     """Defines the root of the app"""
-    return jsonify({'message': 'Bienvenue'})
+    return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
@@ -31,5 +31,5 @@ def register_user() -> Response:
         return jsonify({'message': 'email already registered'}), 400
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
