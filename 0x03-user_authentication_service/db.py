509ceb96e30960fa02963e41b6a0fa30e19b9391
THOSSYN <alextosin5@gmail.com>
2024-02-22 09:52:04 +0100

    Added only add_user to db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,9 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from user import Base, User
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-from user import Base, User
 
 
 class DB:
@@ -46,9 +46,9 @@ class DB:
 
     def find_user_by(self, **kwargs): -> User:
         """Find user object using keyword from attribute"""
+        session = self._session
         try:
-            # filtering is achieved with filter_by, which uses keyword args
-            user = self._session.query(User).filter_by(**kwargs).first()
+            user = session.query(User).filter_by(**kwargs).first()
             if user is None:
                 raise NoResultFound
             return user
