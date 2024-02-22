f74744a0f1e8e420a5134bad07ed39338f96a345
THOSSYN <alextosin5@gmail.com>
2024-02-22 12:03:32 +0000

    Mended error catching in register_user method in auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 import uuid
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class Auth:
@@ -14,16 +15,17 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """User registration"""
-        existing_user = self._db.find_user_by(email=email)
+        try:
+            existing_user = self._db.find_user_by(email=email)
 
-        if existing_user is not None:
-            raise ValueError(f"User {email} already exists")
+            if existing_user is not None:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            self._db.add_user(email, hashed_password)
+            new_user = User(email=email, hashed_password=hashed_password)
 
-        hashed_password = self._hash_password(password)
-        self._db.add_user(email, hashed_password)
-        new_user = User(email=email, hashed_password=hashed_password)
-
-        return new_user
+            return new_user
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validates login details"""
