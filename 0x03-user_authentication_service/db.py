0f235e72cb53662a08f8cc0eee94bd9631a1b0b0
THOSSYN <alextosin5@gmail.com>
2024-02-22 09:27:53 +0100

    Added only add_user to db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -37,6 +37,8 @@ class DB:
             ) -> User:
         """Add a user to the database"""
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        if user:
+            self._session.add(user)
+            self._session.commit()
+            return user
+        return None
