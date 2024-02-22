370caf647de988f02b39b15acb3102a910609caf
THOSSYN <alextosin5@gmail.com>
2024-02-22 08:52:05 +0100

    Added only add_user to db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,47 +34,9 @@ class DB:
             self,
             email: str,
             hashed_password: str
-            ) -> TypeVar('User'):
+            ) -> User:
         """Add a user to the database"""
         user = User(email, hashed_password)
         seld.__session.add(user)
         self.__session.commit()
         return user
-
-    def find_user_by(self, **kwargs) -> str:
-        """Filter user by keyword argument(attributes)
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound("Not found")
-            return user
-        except InvalidRequestError as e:
-            print(e)
-            raise InvalidRequestError("Invalid")
-        """
-        # try:
-        user = self._session.query(User).filter_by(**kwargs).first()
-        # if user is None:
-            # raise ValueError("User Not in DB")
-        return user
-
-
-    def update_user(self, user_id: str, **kwargs) -> None:
-        """Update a user object"""
-        found_user = self.find_user_by(id=user_id)
-        if not found_user:
-            return None
-        if not kwargs:
-            raise ValueError("Attribute does not exist")
-
-        for k, v in kwargs.items():
-            if not hasattr(found_user, k):
-                raise ValueError(f"Attribute {k} does not exist")
-            setattr(found_user, k, v)
-        self._session.commit()
-
-    def _hash_password(self, password: str) -> bytes:
-        """Password-hashing method"""
-        encoded_pwd = password.encode('utf-8')
-        salt_algo = bcrypt.gensalt()
-        return bcrypt.hashpw(encoded_pwd, salt_algo)
