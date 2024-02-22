3787e9257ea2ff2451dad4bdd5b923f6b05b6dd6
THOSSYN <alextosin5@gmail.com>
2024-02-22 12:00:44 +0100

    Update db.py for error raising

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -19,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -44,15 +44,15 @@ class DB:
 
     def find_user_by(self, **kwargs) -> str:
         """Filter user by keyword argument(attributes)"""
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound
-            return user
-        except InvalidRequestError as e:
-            raise e
-        except NoResultFound as e:
-            raise e
+        # try:
+        user = self._session.query(User).filter_by(**kwargs).first()
+            # if user is None:
+                # raise NoResultFound
+        return user
+        # except InvalidRequestError as e:
+            # raise e
+        # except NoResultFound as e:
+            # raise e
 
     def update_user(self, user_id: str, **kwargs) -> None:
         """Update a user object"""
