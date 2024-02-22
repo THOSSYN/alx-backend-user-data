edeaf074670c7fa2181fe686bbff4689421e8eb7
THOSSYN <alextosin5@gmail.com>
2024-02-22 15:45:45 +0000

    Updated auth.py get_reset

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,12 +85,18 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
         return None
 
-    def get_reset_password_token(email: str) -> str:
-        """A password resetting logic"""
-        try:
-            user = self._db.find_user_by(email=email)
-            u_id = _generate_user_id()
-            self._db.update_user(user.id, reset_token=u_id)
-            return u_id
-        except NoResultFound:
-            raise ValueError
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        This method sets the reset_token attribute of the user in
+        the database.
+        """
+        if email:
+            try:
+                user = self._db.find_user_by(email=email)
+                reset_token = _generate_uuid()
+                self._db.update_user(user.id, reset_token=reset_token)
+                return reset_token
+            except NoResultFound:
+                raise ValueError
+
+        return None
