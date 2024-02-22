032a0f31b88d2510625bb59abb52bc63453eae9d
THOSSYN <alextosin5@gmail.com>
2024-02-22 11:05:13 +0100

    Updated db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,16 +33,14 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(
-            self,
-            email: str,
-            hashed_password: str
-            ) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database"""
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        if email and hashed_password:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+            return user
+        return None
 
     def find_user_by(self, **kwargs) -> str:
         """Filter user by keyword argument(attributes)"""
